bb0075fce03059849d2f971a10dc5318ff1ee3dd
Johannes Braun <braun.johannes@gmail.com>
2014-12-17 21:12:07 +0000

    read html v1

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var express = require('express')
-var app = express();
+#!/usr/bin/env node
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var fs=require('fs');
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!')
-})
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
