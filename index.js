43bb11e49840651a9b6bbc663812ed54c2494f4c
Johannes Braun <braun.johannes@gmail.com>
2014-12-17 20:53:44 +0000

    reset index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var outfile = "hello.txt";
-var out = "A startup is a business built to grow rapidly.\n";
-fs.writeFileSync(outfile, out);  
-console.log("Script: " + __filename + "\nWrote: " + out + "To: " + outfile);
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World2!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
